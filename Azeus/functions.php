0d291d8fc1aafb51fdabdf870dac27ac6c843b92
“0thunder0” <myth@7liege.cc>
2018-10-19 06:20:09 +0000

    20181019

--- Azeus/functions.php
+++ Azeus/functions.php
@@ -72,36 +72,176 @@ function register_my_menus() {
 add_action( 'init', 'register_my_menus' );
 // ------------------------------------------------------------------------------
 //添加自定义图集
-add_action('init','my_style_post');
-function my_style_post(){
+function movietalk_custom_post_type_movie() {
   $labels = array(
-    'name'               => '图集',
-      'singular_name'      => '图集',
-      'add_new'            => '添加相册',
-      'add_new_item'       => '添加相册资料',
-      'edit_item'          => '编辑相册资料',
-      'new_item'           => '新相册',
-      'all_items'          => '所有相册',
-      'view_item'          => '查看相册',
-      'search_items'       => '搜索相册',
-      'not_found'          => '没找到相册',
-      'not_found_in_trash' => '回收站里没找到相册资料', 
-      'menu_name'          => '图集'
-  );
-  $supports=array( 
-    'title', 
-    'editor', 
-    'thumbnail'
+    'name'               => '电影',
+    'singular_name'      => '电影',
+    'add_new'            => '添加电影',
+    'add_new_item'       => '添加电影资料',
+    'edit_item'          => '编辑电影资料',
+    'new_item'           => '新电影',
+    'all_items'          => '所有电影',
+    'view_item'          => '查看电影',
+    'search_items'       => '搜索电影',
+    'not_found'          => '没找到电影资料',
+    'not_found_in_trash' => '回收站里没找到电影资料', 
+    'menu_name'          => '电影'
   );
   $args = array(
-    'public'=>true,
-    'labels'=>$labels,
+    'public'        => true,
+    'labels'        => $labels,
     'menu_position' => 5,
-    'supports'      => $supports,
+    'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpt', 'custom-fields', 'revisions' ),
     'has_archive'   => true,
-    'rewrite'       => array( 'slug'  => 'pic_club', 'with_front'  => false ),
+    'rewrite'       => array( 'slug'  => 'movie', 'with_front'  => false ),
+   );
+  register_post_type( 'movie', $args );
+}
+add_action( 'init', 'movietalk_custom_post_type_movie' );
+function my_taxonomies_movie() {
+  $labels = array(
+    'name'              => _x( '电影分类', 'taxonomy 名称' ),
+    'singular_name'     => _x( '电影分类', 'taxonomy 单数名称' ),
+    'search_items'      => __( '搜索电影分类' ),
+    'all_items'         => __( '所有电影分类' ),
+    'parent_item'       => __( '该电影分类的上级分类' ),
+    'parent_item_colon' => __( '该电影分类的上级分类：' ),
+    'edit_item'         => __( '编辑电影分类' ),
+    'update_item'       => __( '更新电影分类' ),
+    'add_new_item'      => __( '添加新的电影分类' ),
+    'new_item_name'     => __( '新电影分类' ),
+    'menu_name'         => __( '电影分类' ),
   );
-  register_post_type('pic_club', $args );
+  $args = array(
+    'labels' => $labels,
+    'hierarchical' => true,
+  );
+  register_taxonomy( 'movie_category', 'movie', $args );
+}
+add_action( 'init', 'my_taxonomies_movie', 0 );
+add_action( 'add_meta_boxes', 'movie_director' );
+function movie_director() {
+    add_meta_box(
+        'movie_director',
+        '电影导演',
+        'movie_director_meta_box',
+        'movie',
+        'side',
+        'low'
+    );
+}
+function movie_director_meta_box($post) {
+ 
+    // 创建临时隐藏表单，为了安全
+    wp_nonce_field( 'movie_director_meta_box', 'movie_director_meta_box_nonce' );
+    // 获取之前存储的值
+    $value = get_post_meta( $post->ID, '_movie_director', true );
+ 
+    ?>
+ 
+    <label for="movie_director"></label>
+    <input type="text" id="movie_director" name="movie_director" value="<?php echo esc_attr( $value ); ?>" placeholder="输入导演名称" >
+ 
+    <?php
+}
+function movie_director_save_meta_box($post_id){
+ 
+    // 安全检查
+    // 检查是否发送了一次性隐藏表单内容（判断是否为第三者模拟提交）
+    if ( ! isset( $_POST['movie_director_meta_box_nonce'] ) ) {
+        return;
+    }
+    // 判断隐藏表单的值与之前是否相同
+    if ( ! wp_verify_nonce( $_POST['movie_director_meta_box_nonce'], 'movie_director_meta_box' ) ) {
+        return;
+    }
+    // 判断该用户是否有权限
+    if ( ! current_user_can( 'edit_post', $post_id ) ) {
+        return;
+    }
+ 
+    // 判断 Meta Box 是否为空
+    if ( ! isset( $_POST['movie_director'] ) ) {
+        return;
+    }
+ 
+    $movie_director = sanitize_text_field( $_POST['movie_director'] );
+    update_post_meta( $post_id, '_movie_director', $movie_director );
+ 
+}
+add_action("manage_posts_custom_column",  "movie_custom_columns");
+add_filter("manage_edit-movie_columns", "movie_edit_columns");
+function movie_custom_columns($column){
+    global $post;
+    switch ($column) {
+        case "movie_director":
+            echo get_post_meta( $post->ID, '_movie_director', true );
+            break;
+    }
+}
+function movie_edit_columns($columns){
+ 
+    $columns['movie_director'] = '导演';
+ 
+    return $columns;
+}
+// ------------------------------------------------------------------------------
+//添加相册功能
+function git_gallery() {
+  $labels = array(
+    'name'                  => '相册',
+    'singular_name'         => '相册',
+    'menu_name'             => '相册',
+    'name_admin_bar'        => '相册',
+    'archives'              => '相册分类',
+    'parent_item_colon'     => '父分类',
+    'all_items'             => '所有相册',
+    'add_new_item'          => '添加新的相册',
+    'add_new'               => '添加新相册',
+    'new_item'              => '新相册',
+    'edit_item'             => '编辑相册',
+    'update_item'           => '更新相册',
+    'view_item'             => '查看相册',
+    'search_items'          => '搜索相册',
+    'not_found'             => '没有相册',
+    'not_found_in_trash'    => '回收站内没有相册',
+    'featured_image'        => '特色图片',
+    'set_featured_image'    => '设置特色图片',
+    'remove_featured_image' => '移除特色图片',
+    'use_featured_image'    => '设为特色图片',
+    'insert_into_item'      => '插入相册',
+    'uploaded_to_this_item' => '上传',
+    'items_list'            => '相册列表',
+    'items_list_navigation' => '相册列表导航',
+    'filter_items_list'     => '筛选相册列表',
+  );
+  $rewrite = array(
+    'slug'                  => 'gallery',
+    'with_front'            => true,
+    'pages'                 => true,
+    'feeds'                 => true,
+  );
+  $args = array(
+    'label'                 => '相册',
+    'description'           => '相册功能',
+    'labels'                => $labels,
+    'supports'              => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'custom-fields', 'post-formats', ),
+    'hierarchical'          => false,
+    'public'                => true,
+    'show_ui'               => true,
+    'show_in_menu'          => true,
+    'menu_position'         => 5,
+    'menu_icon'             => 'dashicons-format-gallery',
+    'show_in_admin_bar'     => true,
+    'show_in_nav_menus'     => true,
+    'can_export'            => true,
+    'has_archive'           => 'gallery',
+    'exclude_from_search'   => true,
+    'publicly_queryable'    => true,
+    'rewrite'               => $rewrite,
+    'capability_type'       => 'page',
+  );
+  register_post_type( 'gallery', $args );
 //添加图集分类
     $labels2 = array(
     'name'              => _x( '相册分类', 'taxonomy 名称' ),
@@ -120,83 +260,30 @@ function my_style_post(){
     'labels' => $labels2,
     'hierarchical' => true,
   );
-  register_taxonomy( 'pic_club_category', 'pic_club', $args2 );
- 
-};
-// $label数组告诉wordpress如何显示这个文章类型的相关信息
-// $supports数组告诉wordpress这个文章类型可以支持什么（比如标题title,编辑栏editor,特色图片,revisions,自定义栏目custom-fields,摘要excerpt）
-
-//丰富自定义文章可以输入的内容，添加自定义 Meta Box
-add_action('add_meta_boxes','pic_club_meta');
-function pic_club_meta(){
-  //add_meta_box( $id, $title, $callback, $post_type, $context,$priority, $callback_args );
-  add_meta_box(
-    'pic_club_meta',
-    '相册关键词',
-    'pic_meta_func',
-    'pic_club',
-    'side',
-    //侧边栏（side）。因为内容不多，所以侧边栏足够，若内容较多，可以将“side”改为“advanced”，这样就会在主内容区域渲染Meta Box
-      'low'
-  );
-};
-
-function pic_meta_func($post) { 
-    // 创建临时隐藏表单，为了安全
-    wp_nonce_field( 'pic_meta_func', 'pic_meta_func_nonce' );
-    // 获取之前存储的值
-    $value = get_post_meta( $post->ID, '_pic_club_meta', true );
-?>
-     <label for="pic_club_meta"></label>
-    <input type="text" id="pic_club_meta" name="pic_club_meta" value="<?php echo esc_attr( $value ); ?>" placeholder="输入导演名称" >
- 
-    <?php
-};
-//保存Meta Box 表单内容
-function pic_club_save_meta_box($post_id){
- 
-    // 安全检查
-    // 检查是否发送了一次性隐藏表单内容（判断是否为第三者模拟提交）
-    if ( ! isset( $_POST['pic_meta_func_nonce'] ) ) {
-        return;
-    }
-    // 判断隐藏表单的值与之前是否相同
-    if ( ! wp_verify_nonce( $_POST['pic_meta_func_nonce'], 'pic_meta_func' ) ) {
-        return;
-    }
-    // 判断该用户是否有权限
-    if ( ! current_user_can( 'edit_post', $post_id ) ) {
-        return;
-    }
- 
-    // 判断 Meta Box 是否为空
-    if ( ! isset( $_POST['pic_club_meta'] ) ) {
-        return;
-    }
- 
-    $pic_club_meta = sanitize_text_field( $_POST['pic_club_meta'] );
-    update_post_meta( $post_id, '_pic_club_meta', $pic_club_meta );
- 
+  register_taxonomy( 'gallery_category', 'gallery', $args2 );
 }
+add_action( 'init', 'git_gallery', 0 );
 
-//列表显示自定义Meta Box字段
-add_action("manage_posts_custom_column",  "movie_custom_columns");
-add_filter("manage_edit-movie_columns", "movie_edit_columns");
-function movie_custom_columns($column){
-    global $post;
-    switch ($column) {
-        case "pic_club_meta":
-            echo get_post_meta( $post->ID, '_pic_club_meta', true );
-            break;
-    }
+//相册的固定连接格式
+function custom_gallery_link( $link, $post = 0 ){
+  if ( $post->post_type == 'gallery' ){
+    return home_url( 'gallery/' . $post->ID .'.html' );
+  } else {
+    return $link;
+  }
 }
-function movie_edit_columns($columns){
- 
-    $columns['pic_club_meta'] = '导演';
- 
-    return $columns;
+add_filter('post_type_link', 'custom_gallery_link', 1, 3);
+function custom_gallery_rewrites_init(){
+  add_rewrite_rule(
+    'gallery/([0-9]+)?.html$',
+    'index.php?post_type=gallery&p=$matches[1]',
+    'top' );
 }
+add_action( 'init', 'custom_gallery_rewrites_init' );
+
+
 // ------------------------------------------------------------------------------
+
 //去除分类标志代码
 add_action( 'load-themes.php',  'no_category_base_refresh_rules');
 add_action('created_category', 'no_category_base_refresh_rules');
